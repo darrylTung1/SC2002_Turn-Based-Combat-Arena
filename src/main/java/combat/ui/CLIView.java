7479b6ce66d1547adde5a74f87e3dfe7bbbe574e
IrmuunCh <112145730+IrmuunCh@users.noreply.github.com>
2026-04-11 21:11:44 +0800

    Update CLIView.java

--- src/main/java/combat/ui/CLIView.java
+++ src/main/java/combat/ui/CLIView.java
@@ -120,7 +120,37 @@ public class CLIView implements GameUI {
     @Override
     public void displayActionResult(Combatant actor, Action action, BattleContext context) {
         System.out.println(actor.getName() + " used " + action.getName() + ".");
-        // TODO: Add detailed damage/effect reporting
+        Combatant target;
+        int damage;
+        int newHp;
+        if(action instanceof BasicAttack){
+            if(actor instanceof Player){
+                target = ((BasicAttack) action).getTarget();
+                damage = Math.max(0, actor.getAttack() - target.getDefense());
+                newHp = Math.max(0, target.getHp() - damage);
+                System.out.println(target.getName() + ": HP: " + target.getHp() + " -> " + newHp + " (dmg : " + actor.getAttack() + " - " + target.getDefense() + " = " + damage + ")");
+            }
+            else{ // must be Enemy
+                target = ((BasicAttack) action).getTarget();
+                damage = Math.max(0, actor.getAttack() - target.getDefense());
+                newHp = Math.max(0, target.getHp() - damage);
+                if(context.isSmokeBombActive() == true){
+                    damage = 0;
+                    newHp = Math.max(0, target.getHp() - damage);
+                    System.out.println(target.getName() + ": HP: " + target.getHp() + " -> " + newHp + " (dmg : 0, Player has a Smoke effect)");
+                }
+                else{
+                    System.out.println(target.getName() + ": HP: " + target.getHp() + " -> " + newHp + " (dmg : " + actor.getAttack() + " - " + target.getDefense() + " = " + damage + ")");
+                }
+            }
+        }
+        if(action instanceof UseItemAction){
+            Item item=((UseItemAction) action).getSelectedItem();
+            if(item.getName()=="Potion"){
+                newHp = Math.min(actor.getMaxHp(), actor.getHp() + 100);
+                System.out.println(actor.getName() + ": HP: " + actor.getHp() + " -> " + newHp);
+            }
+        }
     }
 
     // =============================================
